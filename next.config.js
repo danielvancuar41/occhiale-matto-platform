8d27ebfd1dfea16ada4dc91f66e355915d4d6a0d
danielvancuar41 <vanacoredaniele99@gmail.com>
2026-09-01 16:00:01 +0200

    Add remote pattern for www.occhialematto.com

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "occhialematto.com" },
+      { protocol: "https", hostname: "www.occhialematto.com" },
       { protocol: "https", hostname: "cdn.shopify.com" }
     ]
   }
